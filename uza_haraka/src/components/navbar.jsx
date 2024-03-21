0ef9f975f9d0d5ee04b97dd2ea4b905158a390c8
Peter Rigii <theonlyrigii@gmail.com>
2024-03-21 14:39:38 +0300

    fixed navbar

--- uza_haraka/src/components/navbar.jsx
+++ uza_haraka/src/components/navbar.jsx
@@ -1,61 +1,31 @@
-import {
-    Avatar,
-    Button,
-    Dropdown,
-    DropdownDivider,
-    DropdownHeader,
-    DropdownItem,
-    Navbar,
-    NavbarBrand,
-    NavbarCollapse,
-    NavbarLink,
-    DarkThemeToggle,
-    
-  } from 'flowbite-react';
-  import { FormControl, Flex, Spacer, Box } from '@chakra-ui/react';
-  import { useNavigate, useParams } from 'react-router-dom';
- 
-  function NavBar() {
-    
-    let navigate=useNavigate()
-    let { username } = useParams()
-    return (
-      <Navbar className="w-full">
-        
-        <div className="flex flex-column items-center justify-center">
-          <Flex>
-            <NavbarBrand href="#">
-              <span className=" whitespace-nowrap ml-4 text-xl text-teal-400 font-semibold dark:text-teal-400">UzaHaraka</span>
-            </NavbarBrand> 
-            <FormControl className=' md:w-auto'>
-              <input type="text" placeholder="Search Products" className="border-2 border-gray-400 rounded-xl w-full ml-4 px-4 py-2" />
-            </FormControl>
-            <Spacer />
-            <Dropdown
-                  className='justify-flex-end'
-                  arrowIcon={false}
-                  inline
-                  label={
-                    <Avatar alt="User settings" img="" rounded />
-                  }
-                >
-                  <DropdownHeader>
-                    <span className="block text-sm">{username}</span>
-                    <span className="block truncate text-sm font-medium">{username}</span>
-                  </DropdownHeader>
-                  <DropdownItem>Dashboard</DropdownItem>
-                  <DropdownItem>Settings</DropdownItem>
-                  <DropdownItem>Earnings</DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem onClick={()=>{navigate('/')}}>Sign out</DropdownItem>
-                </Dropdown>
-                <DarkThemeToggle />
-          </Flex>
-        </div>
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+
+function NavBar() {
+  return (
+    <Navbar fluid rounded className='bg-teal-400 shadow-lg mb-4 w-full'>
+      <Navbar.Brand href="https://flowbite-react.com">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">UzaHaraka</span>
+      </Navbar.Brand>
+      <input type="text" className="rounded-8" placeholder='Search Products' />
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+          }
+        >
+          
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
       
-        
-       
-      </Navbar>
-    );
-  }
-  export default NavBar
+    </Navbar>
+  );
+}
+export default NavBar;
