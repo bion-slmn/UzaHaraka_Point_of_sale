e72c784ae98d4f4318ec9fe269b501cfcdc6eff1
bion-slmn <bionsol25@gmail.com>
2024-03-28 17:11:40 +0300

    adding dos string in the signals to create a profile

--- point_of_sale/pos_users/signals.py
+++ point_of_sale/pos_users/signals.py
@@ -1,11 +1,20 @@
+'''
+create a signal so that when ever a user module is save in the database,
+a profile instance is saved in the databse for that user
+
+'''
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    '''
+    when ever a user is created, a profile is created
+    in the database for that user
+    '''
     if created:
         profile = Profile.objects.create(user=instance)
         profile.save()
-
