2dc2963cffe9f25c546806fdcc9bc29ac9bf0755
bion-slmn <bionsol25@gmail.com>
2024-03-28 17:36:05 +0300

    conforming to pycodestyle standards

--- point_of_sale/inventory/admin.py
+++ point_of_sale/inventory/admin.py
@@ -1,11 +1,18 @@
-from django.contrib import admin
+'''
+registering models so that they appear in the admin page
+also configuring how to display them in the admin page
+'''
 
+from django.contrib import admin
 from .models import Product, Sales, Category, Purchase
+
+
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Product, ProductAdmin)
+
 
 class SalesAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'created_at', 'quantity', 'selling_price', 'total', 'user_name')
@@ -26,6 +33,7 @@ class CategoryAdmin(admin.ModelAdmin):
         return Product.objects.filter(category=obj).count()
 
 admin.site.register(Category, CategoryAdmin)
+
 
 class PurchaseAdmin(admin.ModelAdmin):
     pass
