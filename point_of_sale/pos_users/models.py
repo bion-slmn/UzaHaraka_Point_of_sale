890fd11fa3f478522ec2e3c92d78f493352c0c62
bion-slmn <bionsol25@gmail.com>
2024-03-28 17:07:05 +0300

    adding docstrings and conforming to pycodestyle standards for the pos_user/model.py

--- point_of_sale/pos_users/models.py
+++ point_of_sale/pos_users/models.py
@@ -1,12 +1,24 @@
+'''
+creating profile model. this model is created automatically for each user
+after the user is created using signals
+'''
 from django.db import models
 from django.contrib.auth.models import User
 
 
 # create a profile of a user
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    phone_number = models.CharField(max_length=15, null=True) 
-    profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    user = models.OneToOneField(
+                                User,
+                                on_delete=models.CASCADE,
+                                related_name='profile'
+                                )
+    phone_number = models.CharField(max_length=15, null=True)
+    profile_pic = models.ImageField(
+                                    upload_to='profile_pics/',
+                                    null=True,
+                                    blank=True
+                                    )
 
     def __str__(self):
         return self.user.username
